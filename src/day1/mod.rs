0749cd0f47e2ad7ab2c5d3e1a98da7151b4ee0dc
Francisco Gonzalez Isas <francisco@frgois.com>
2024-12-02 07:45:59 -0600

    Clean up day 1

--- src/day1/mod.rs
+++ src/day1/mod.rs
@@ -1,4 +1,4 @@
-pub fn get_distance() -> (i32, i32) {
+pub fn get_distance_and_similarity() -> (i32, i32) {
     let input = include_str!("./inputs.txt");
     let (mut list1, mut list2) = (Vec::new(), Vec::new());
     let mut distance = 0;
@@ -20,7 +20,6 @@ pub fn get_distance() -> (i32, i32) {
 pub fn get_similarity(list1: &[i32], list2: &[i32]) -> i32 {
     let mut similarity_score = 0;
 
-    // list1 y list2 ya están ordenados
     list1.into_iter().for_each(|n| {
         let similar = list2.into_iter().filter(|&m| m == n).collect::<Vec<&i32>>();
         similarity_score += n * similar.len() as i32;
