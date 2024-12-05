ded5ce416ec2697c295a39f38ebe9db511e4e791
Francisco Gonzalez Isas <francisco@frgois.com>
2024-12-05 17:37:40 -0600

    Iterators simplify

--- src/day3/mod.rs
+++ src/day3/mod.rs
@@ -2,17 +2,17 @@ use regex::Regex;
 
 pub fn clean_multiplication_file() -> i32{
     let input = include_str!("./inputs.txt");
-    let mut results: Vec<i32> = vec![];
 
     // parenthesis around \d indicate the groups we want to capture.
     let multi_regex = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)");
 
     // Extract two digits from mul(n1,n2) statements and multiply them together.
     // Store result to perform sum later.
-    for (_, [ n1, n2]) in multi_regex.unwrap().captures_iter(input).map(|c| c.extract()) {
-        results.push(n1.parse::<i32>().unwrap() * n2.parse::<i32>().unwrap());
-    }
+    multi_regex.unwrap().captures_iter(input).map(|c| {
+        let n1 = c.get(1).unwrap().as_str().parse::<i32>().unwrap();
+        let n2 = c.get(2).unwrap().as_str().parse::<i32>().unwrap();
 
-    results.into_iter().sum()
+        n1 * n2
+    }).sum()
 
 }
