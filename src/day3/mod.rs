ac6d95a8908626e6ea51b3fbb19ece001b95ae30
Francisco Gonzalez Isas <francisco@frgois.com>
2024-12-05 17:43:37 -0600

    Reduced day3 pt1 solution size.

--- src/day3/mod.rs
+++ src/day3/mod.rs
@@ -7,12 +7,9 @@ pub fn clean_multiplication_file() -> i32{
     let multi_regex = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)");
 
     // Extract two digits from mul(n1,n2) statements and multiply them together.
-    // Store result to perform sum later.
+    // Return the sum.
     multi_regex.unwrap().captures_iter(input).map(|c| {
-        let n1 = c.get(1).unwrap().as_str().parse::<i32>().unwrap();
-        let n2 = c.get(2).unwrap().as_str().parse::<i32>().unwrap();
-
-        n1 * n2
+        let (_, [n1, n2]) = c.extract();
+        n1.parse::<i32>().unwrap() * n2.parse::<i32>().unwrap()
     }).sum()
-
 }
